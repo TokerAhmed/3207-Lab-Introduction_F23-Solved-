28bbd52365f641dad3b8262afd4c3c292d5ed6a8
“TokerAhmed” <tup19966@temple.edu>
2023-09-01 23:26:01 -0400

    This is the updated print_random.c with comments

--- print_random.c
+++ print_random.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include "random.h"
 
 
 int main()
 {
-	int a;
+	int a; //declare a integer variable a 
 
-	/* seed the randomizer */
+	// seed the randomizer
 	srand( (unsigned)time(NULL) );
 
 	printf("Today's random word: ");
+
+	//using a for loop to get seven  random characters from our randchar() function
 	for(a=0;a<7;a++)
 		putchar( randchar() );
 	putchar('\n');
