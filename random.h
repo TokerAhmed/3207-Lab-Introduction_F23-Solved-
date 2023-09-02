b9e629e808fc95c1b3f837ad5e2e87712c5b6e20
“TokerAhmed” <tup19966@temple.edu>
2023-09-01 23:29:52 -0400

    This is the header file random.h

---
+++ random.h
@@ -0,0 +1,9 @@
+#if defined(random_char_h)  // random_char_h is defined
+
+#else //if not defined define random_char_h
+
+#define random_char_h
+
+char randchar(void); //the prototype for randchar() function
+
+#endif
